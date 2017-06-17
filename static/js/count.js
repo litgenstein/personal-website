d9a6d478eac25423d5b860e5f291d9f74b3a9649
Jack <me@jarrekk.com>
2017-06-17 15:15:54 +0800

    edit js

--- static/js/count.js
+++ static/js/count.js
@@ -5,12 +5,13 @@ $(document).ready(function() {
         type: "get",
         async: false,
         url: "https://api.jarrekk.com/api/v1.0/jalpc/pv_count",
+        // url: "http://127.0.0.1:5000/api/v1.0/jalpc/pv_count",
         dataType: "jsonp",
         jsonp: "callback",
         jsonpCallback: "flightHandler",
         success: function(json) {
             var website_count = json.data;
-            $("#jalpc_site_pv").html('<span class="navy">' + website_count + '</span>&nbsp;<span data-i18n="link.view">views</span>&nbsp;||&nbsp;' + pre_content);
+            $("#jalpc_site_pv").html("<span class=\"navy\">" + website_count + "</span>&nbsp;<span data-i18n=\"link.view\">views</span>&nbsp;||&nbsp;" + pre_content);
         },
         error: function() {
             console.log('fail');
